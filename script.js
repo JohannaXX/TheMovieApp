cec54b7f8ee1649b1b6181df9584d646ece13adf
JohannaXX <2myadverts@gmail.com>
2019-05-26 21:50:28 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,62 @@
+let poster;
+let title;
+let year;
+let type;
+let imdbID;
+
+let giphyTitle;
+let url;
+let videoID;
+let videos;
+
+document.getElementById('searchForm').addEventListener('submit', function search(e) {
+    document.getElementById('movieList').innerHTML = "";
+    document.getElementById('videoList').innerHTML = "";
+
+    e.preventDefault();
+    let searchWord = document.getElementById('searchText').value;
+    
+    axios.get(`http://www.omdbapi.com?s="${searchWord}"&apikey=9be27fce`)
+        .then((response) => {
+            const results = response.data.Search;
+            console.log(results);
+        
+            for (let i = 0; i<results.length; i++) {
+                poster = results[i].Poster;
+                title = results[i].Title;
+                year = results[i].Year;
+                type = results[i].Type;
+                imdbID = results[i].imdbID;      
+                
+                document.getElementById('movieList').innerHTML += 
+                    `<div class="movieDescription">
+                            <img src="${poster}"/>
+                            <div>
+                                <h3>${title}</h3>
+                                <div>${year}</div>
+                                <div>${type}</div>
+                            </div> 
+                    </div>`; 
+
+                giphyTitle = title.replace(/(:|-|'|,)/, "").toLocaleLowerCase().split(" ").join("-");
+                   
+                const postMovie = axios.get(`http://api.giphy.com/v1/gifs/search?q=movie-${giphyTitle}&api_key=hwi9C9Fit7p3LAAcYwHpK33g4kCeowWl&limit=1`)
+                .then((answer) => {
+                    videos = answer.data.data;
+                    url = videos[0].url;
+                    console.log(url);
+
+                    videoID = url.split("-");
+                    videoID = videoID[videoID.length-1];
+
+                    document.getElementById('videoList').innerHTML += `<iframe src="https://giphy.com/embed/${videoID}" frameBorder="0" scrolling="no"></iframe>`;
+                })
+                .catch((err) => {
+                    console.log(err);
+                })    
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
