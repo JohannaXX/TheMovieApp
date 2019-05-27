c2166603176c54c3be4efa61a9537af218cc9a10
JohannaXX <2myadverts@gmail.com>
2019-05-27 20:40:09 +0200

    keys

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.getElementById('searchForm').addEventListener('submit', function search
     e.preventDefault();
     let searchWord = document.getElementById('searchText').value;
     
-    axios.get(`https://www.omdbapi.com?s="${searchWord}"&apikey=9be27fce`)
+    axios.get(`https://www.omdbapi.com?s="${searchWord}"&apikey=${myomdbkey}`)
         .then((response) => {
             const results = response.data.Search;
             console.log(results);
@@ -40,7 +40,7 @@ document.getElementById('searchForm').addEventListener('submit', function search
 
                 giphyTitle = title.replace(/(:|-|'|,)/, "").toLocaleLowerCase().split(" ").join("-");
                    
-                const postMovie = axios.get(`https://api.giphy.com/v1/gifs/search?q=movie-${giphyTitle}&api_key=hwi9C9Fit7p3LAAcYwHpK33g4kCeowWl&limit=1`)
+                const postMovie = axios.get(`https://api.giphy.com/v1/gifs/search?q=movie-${giphyTitle}&api_key=${mygiphykey}&limit=1`)
                 .then((answer) => {
                     videos = answer.data.data;
                     url = videos[0].url;
@@ -60,3 +60,6 @@ document.getElementById('searchForm').addEventListener('submit', function search
             console.log(err);
         });
 });
+
+var myomdbkey = config.MY_OMDBKEY;
+var mygiphykey = config.MY_GIPHYKEY;
